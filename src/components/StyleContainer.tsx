a7f59ec19c348347c3e5c300465f1fc652721ef4
powerjett <jindai.pjt@raycloud.com>
2023-03-31 15:51:26 +0800

    create repo

---
+++ src/components/StyleContainer.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+// @ts-ignore
+import txt from '!!raw-loader!../styles/style0.css';
+console.log(txt)
+const StyleContainer:FC = () => {
+  return (
+    <pre id="style-container">
+      color: red;
+    </pre>
+  )
+}
+
+export default StyleContainer
---
+++ src/components/WorkContainer.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+const WorkContainer:FC = () => {
+  return (
+    <div id="work-container" className='flipped'>
+      1asdasdasdasdas\
+      asdasd
+      asdas
+    </div>
+  )
+}
+
+export default WorkContainer
